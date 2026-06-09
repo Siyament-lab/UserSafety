97c71cc942877aaeda733e4ab2fa4f83a8b91127
siboo <siboo3@hotmail.com>
2026-06-09 19:26:07 +0200

    feat: add DTO RegisterRequest with username and password

---
+++ UserSafety/DTOs/RegisterRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserSafetyAPI.DTOs;
+
+public class RegisterRequest
+{
+    [Required (ErrorMessage = "Användarnamn krävs.")]
+    [StringLength (50, MinimumLength = 5, ErrorMessage = "Användarnamn måste vara mellan 5 och 50 tecken.")]
+    public string Username { get; set; } = string.Empty;
+
+    [Required (ErrorMessage = "Lösenord krävs.")]
+    [StringLength (100, MinimumLength = 6, ErrorMessage = "Lösenord måste vara minst 6 tecken.")]
+    public string Password { get; set; } = string.Empty;
+}
