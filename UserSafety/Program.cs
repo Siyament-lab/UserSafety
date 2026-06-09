c78a0631f32e842a13b03ee5ce3b84de7b4e0db9
siboo <siboo3@hotmail.com>
2026-06-10 00:48:43 +0200

    feat: register AppDbContext with SQL server in program.cs

--- UserSafety/Program.cs
+++ UserSafety/Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using UserSafetyAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// Registrera DbContext med SQL Server LocalDB
+builder.Services.AddDbContext<AppDbContext> (options =>
+    options.UseSqlServer (builder.Configuration.GetConnectionString ("DefaultConnection")));
 
 var app = builder.Build();
 
