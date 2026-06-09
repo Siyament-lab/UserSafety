8a8d4c0a8e68ec5dd1f65d53f6c1927d805d278f
siboo <siboo3@hotmail.com>
2026-06-10 00:33:30 +0200

    feat: add data attributes to user properities UserName and PasswordHash

--- UserSafety/Entities/User.cs
+++ UserSafety/Entities/User.cs
@@ -1,9 +1,18 @@
-﻿namespace UserSafetyAPI.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UserSafetyAPI.Entities
 {
     public class User
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        [Column(TypeName = "nvarchar(50)")]
         public string UserName { get; set; } = string.Empty;
+        [Required]
+        [MaxLength(256)]
+        [Column(TypeName = "nvarchar(256)")]
         public string PasswordHash { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
