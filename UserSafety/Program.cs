af2c6ec0b7bd3b35ea40587cc88cbf6ae288bf13
siboo <siboo3@hotmail.com>
2026-06-10 02:38:21 +0200

    feat: register jwt authentication in program.cs

--- UserSafety/Program.cs
+++ UserSafety/Program.cs
@@ -1,5 +1,8 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using UserSafetyAPI.Data;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +14,26 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext> (options =>
     options.UseSqlServer (builder.Configuration.GetConnectionString ("DefaultConnection")));
 
+
+//Registera JWT-autentisering
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey (Encoding.UTF8.GetBytes (builder.Configuration["Jwt:Key"]!))
+
+        };
+    });
+
 var app = builder.Build();
+
 //Automatisk migrering vid uppstart
 using (var scope = app.Services.CreateScope())
 {
@@ -22,9 +44,8 @@ using (var scope = app.Services.CreateScope())
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();//Autentisering måste komma före auktorisering för att jwt ska funka!
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
